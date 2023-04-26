e1b1f9d7d4ddc04e2b534dde07a1c00bb365c90c
silviosalza <silviofilipposalza@gmail.com>
2023-04-26 15:40:54 +0200

    proj. scaff

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
-<script >
-
+<script>
+import AppHeader from "./components/AppHeader.vue"
+import AppJumbotron from "./components/AppJumbotron.vue";
+export default{
+  components: {
+    AppHeader,
+    AppJumbotron
+},
+  data(){
+    return{
+      
+    }
+  }
+}
 </script>
 
 <template>
+  <AppHeader/>
+  <AppJumbotron/>
   
 </template>
 
-<style>
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use "./styles/utilities/variables" as *;
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+  props: {
+    navbarOption: String
+  }
+}
+</script>
+
+<template>
+    <div class="header">
+
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/general.scss" as *;
+@use "../styles/utilities/variables" as *;
+
+.header{
+    height: 100px;
+    background-color: $header-color;
+}
+
+
+
+
+</style>
