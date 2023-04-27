89e9693a1f5496a628d896062572d428c5477e1e
silviosalza <silviofilipposalza@gmail.com>
2023-04-27 18:57:37 +0200

    forum section complete

---
+++ src/components/AppForumCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: "AppForumCard",
+    props: {
+        title: String,
+        icon: String,    
+    }
+}
+</script>
+
+<template>
+    <div class="card col-2">
+        <div class="circle d-flex justify-content-center align-items-center">
+            <i :class="icon"></i>
+        </div>
+        <h5>{{ title }}</h5>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/general.scss" as *;
+@use "../styles/utilities/variables" as *;
+
+.card.col-2{
+    margin: 2rem;
+    border: 0;
+    border-top: 3px solid $header_color;
+    border-radius: 0;
+    padding: 3rem 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.circle{
+    height: 6rem;
+    width: 6rem;
+    background-color: $orange_detail; 
+    border-radius: 50%;    
+    i{
+        font-size: 3rem;
+        color: $white_text;
+    }
+}
+
+</style>
--- src/components/AppForums.vue
+++ src/components/AppForums.vue
@@ -1,28 +1,68 @@
 <script>
+import AppForumCard from './AppForumCard.vue';
 
 export default {
     name: "AppForums",
-    props: {
-        title: String,
-        icon: String,
-     
-    }
+    components: {
+        AppForumCard
+    },
+    data(){
+      return{
+        forum: [
+            {
+                title: "News & Community",
+                icon: "fa-solid fa-file"
+            },
+            {
+                title: "Apple Forum",
+                icon: "fa-brands fa-apple"
+            },
+            {
+                title: "Microsoft Forum",
+                icon: "fa-brands fa-windows"
+            },
+            {
+                title: "Android Forum",
+                icon: "fa-brands fa-android"
+            },
+            {
+                title: "General Discussion",
+                icon: "fa-solid fa-mug-saucer"
+            },
+            {
+                title: "Apps & Software",
+                icon: "fa-solid fa-cubes"
+            },
+            {
+                title: "Gadgets & Stuff",
+                icon: "fa-solid fa-laptop"
+            },
+            {
+                title: "Tutorials & Guides",
+                icon: "fa-solid fa-wrench"
+            },
+        ],
+      }
+    },
 }
 
 </script>
 
 <template>
-    <div class="container">
-        <section class="title_section d-flex flex-column align-items-center gap-3">
-          <h6>AVADA TECH FORUM</h6>
-          <h2>Forum Sections</h2>
-          <div class="pseudoelem_orange"></div>
-        </section>
+    <div class="forum-section">
+        <div class="container pb-4">
+            <section class="title_section d-flex flex-column align-items-center gap-3">
+              <h6>AVADA TECH FORUM</h6>
+              <h2>Forum Sections</h2>
+              <div class="pseudoelem_orange"></div>
+            </section>
+            <div class="row d-flex justify-content-center">
+                <template v-for="(forum,index) in forum" :key="index">
+                <AppForumCard :title = "forum.title" :icon="forum.icon"/>
+              </template>
+            </div>
+        </div>
     </div>
-
-
-
-
     <div class="section_footer d-flex justify-content-center align-items-center">
         <button type="button" class="btn btn-dark"><span>GO TO OUR FORUM</span><i class="fa-solid fa-arrow-right"></i></button>
     </div>
@@ -31,6 +71,16 @@ export default {
 <style scoped lang="scss">
 @use "../styles/general.scss" as *;
 @use "../styles/utilities/variables" as *;
+
+.forum-section{
+    background-color: #f2f2f2;
+}
+.row{
+    padding: 0;
+    margin: 0;
+    width: 100%;
+}
+
 
 
 </style>
